e461eb66f5cce8e6574953e836319a53996c878b
wkolby <wkolby@gmail.com>
2024-09-05 14:54:01 -0600

    Added vascular plant processing

---
+++ Scripts/Level0_Vascular_Processing_Spectra_FuncLevel.R
@@ -0,0 +1,212 @@
+setwd("/Users/wksmith/Documents/GitHub/NASA_EMIT_Biocrust")
+github_dir <- "/Users/wksmith/Documents/GitHub/NASA_EMIT_Biocrust/"
+library(asdreader)
+library(reshape2)
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(stringr)
+
+###FUNCTIONS##################################################################################
+#Calculates vegetation simple ratio (a:b) / (c:d)
+calc_SR <- function(df, a, b, c, d){
+  b1_1 <- which.min(abs(a - as.numeric(names(df), options(warn=-1))))   #Identify first band and start range
+  b1_2 <- which.min(abs(b - as.numeric(names(df), options(warn=-1))))   #Identify first band and end range
+  b2_1 <- which.min(abs(c - as.numeric(names(df), options(warn=-1))))   #Identify second band and start range
+  b2_2 <- which.min(abs(d - as.numeric(names(df), options(warn=-1))))   #Identify second band and end range
+  (rowMeans(df_wide[b1_1:b1_2])/(rowMeans(df_wide[b2_1:b2_2])))
+}
+#Calculates normalized index using (a:b - c:d) / (a:b + c:d)
+calc_VI <- function(df, a, b, c, d){
+  b1_1 <- which.min(abs(a - as.numeric(names(df), options(warn=-1))))   #Identify first band and start range
+  b1_2 <- which.min(abs(b - as.numeric(names(df), options(warn=-1))))   #Identify first band and end range
+  b2_1 <- which.min(abs(c - as.numeric(names(df), options(warn=-1))))   #Identify second band and start range
+  b2_2 <- which.min(abs(d - as.numeric(names(df), options(warn=-1))))   #Identify second band and end range
+  ((rowMeans(df_wide[b1_1:b1_2]) - rowMeans(df_wide[b2_1:b2_2])) /      #vegetation index equation
+      (rowMeans(df_wide[b1_1:b1_2]) + rowMeans(df_wide[b2_1:b2_2])))
+}
+
+#####################################IC1#######################################
+master <- read.csv2(paste(github_dir,'Data/NASA_EMIT_Campaign_062024/Data_Sheets/Vascular/Datasheet_Vascular_IC1_06072024.csv',sep=''),sep=',',header=T)
+master$File<-str_pad(master$File, 5, pad = "0")
+dataset <- data.frame()
+
+for(i in 1:length(master$File)){
+  file <- list.files(paste(github_dir,'/Data/NASA_EMIT_Campaign_062024/IC1_veg',sep=''), full.names = T, pattern = paste(master$File[i],".asd",sep=''))
+  print(file)
+  md<-get_metadata(file)
+  temp_data<-get_spectra(file)
+  data<-as.numeric(temp_data)
+  wvl=as.integer(colnames(temp_data))
+  c1=data[which(wvl==1000)]-data[which(wvl==1001)]
+  c2=data[which(wvl==1800)]-data[which(wvl==1801)]
+  data_c=c(data[1:which(wvl==1000)],data[which(wvl==1001):which(wvl==1800)]+c1,data[which(wvl==1801):which(wvl==2500)]+c1+c2)
+  data_s=data_c/sqrt(sum(data_c^2))
+  type=rep(master$Plant[i],length(wvl))
+  
+  ds=cbind(wvl,data_c,type)
+  dataset <- rbind(dataset,ds)
+}
+
+#Name the columns
+colnames(dataset) <- c("wavelength","reflectance","type")
+dataset=transform(dataset,wavelength = as.numeric(wavelength))
+dataset=transform(dataset,reflectance = as.numeric(reflectance))
+
+#####ASD Reflectance Plots#################
+###Isolate plant signatures
+vascular<-subset(dataset, type %in% c('ATCA','ATCO','ARSP','GUSA','CORA','EPTO','ERNA','KRLA','SAVE','JUOS'))
+vascular<-subset(dataset, type %in% c('KRLA','GUSA','ATCA','ATCO','SAVE'))
+vascular_mean_std <- vascular %>%
+  group_by(wavelength,type) %>%
+  summarise_at(vars(reflectance), list(mean=mean, sd=sd)) %>%
+  as.data.frame()
+
+vascular_mean_std$type <- factor(vascular_mean_std$type,levels=c('KRLA','GUSA','ATCA','ATCO','SAVE')) #Set the order for manual fill
+ggplot(vascular_mean_std, aes(x=wavelength,y=mean,group=type,color=type)) +
+  geom_line(show.legend = T,linewidth=.5,linetype="solid") +
+  geom_ribbon(aes(y = mean, ymin = mean - sd, ymax = mean + sd, fill = type), alpha = .2) +
+  scale_y_continuous("Reflectance") +
+  scale_x_continuous("Wavelength (nm)",limits = c(400,2400), breaks = seq(400,2400,200)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+  theme(text = element_text(size = 18))
+ggsave(paste(github_dir,'/figures/IC1_Full_Vascular.png',sep=''),dpi=300,width=180,height=120,units='mm')
+
+#####################################IC2#######################################
+master <- read.csv2(paste(github_dir,'Data/NASA_EMIT_Campaign_062024/Data_Sheets/Vascular/Datasheet_Vascular_IC2_06062024.csv',sep=''),sep=',',header=T)
+master$File<-str_pad(master$File, 5, pad = "0")
+dataset <- data.frame()
+
+for(i in 1:length(master$File)){
+  file <- list.files(paste(github_dir,'/Data/NASA_EMIT_Campaign_062024/IC2_veg',sep=''), full.names = T, pattern = paste(master$File[i],".asd",sep=''))
+  print(file)
+  md<-get_metadata(file)
+  temp_data<-get_spectra(file)
+  data<-as.numeric(temp_data)
+  wvl=as.integer(colnames(temp_data))
+  c1=data[which(wvl==1000)]-data[which(wvl==1001)]
+  c2=data[which(wvl==1800)]-data[which(wvl==1801)]
+  data_c=c(data[1:which(wvl==1000)],data[which(wvl==1001):which(wvl==1800)]+c1,data[which(wvl==1801):which(wvl==2500)]+c1+c2)
+  data_s=data_c/sqrt(sum(data_c^2))
+  type=rep(master$Plant[i],length(wvl))
+  
+  ds=cbind(wvl,data_c,type)
+  dataset <- rbind(dataset,ds)
+}
+
+#Name the columns
+colnames(dataset) <- c("wavelength","reflectance","type")
+dataset=transform(dataset,wavelength = as.numeric(wavelength))
+dataset=transform(dataset,reflectance = as.numeric(reflectance))
+
+#####ASD Reflectance Plots#################
+###Isolate plant signatures
+vascular<-subset(dataset, type %in% c('ATCA','ATCO','GUSA','KRLA','SAVE'))
+vascular<-subset(dataset, type %in% c('KRLA','GUSA','ATCA','ATCO','SAVE'))
+vascular_mean_std <- vascular %>%
+  group_by(wavelength,type) %>%
+  summarise_at(vars(reflectance), list(mean=mean, sd=sd)) %>%
+  as.data.frame()
+
+vascular_mean_std$type <- factor(vascular_mean_std$type,levels=c('KRLA','GUSA','ATCA','ATCO','SAVE')) #Set the order for manual fill
+ggplot(vascular_mean_std, aes(x=wavelength,y=mean,group=type,color=type)) +
+  geom_line(show.legend = T,linewidth=.5,linetype="solid") +
+  geom_ribbon(aes(y = mean, ymin = mean - sd, ymax = mean + sd, fill = type), alpha = .2) +
+  scale_y_continuous("Reflectance") +
+  scale_x_continuous("Wavelength (nm)",limits = c(400,2400), breaks = seq(400,2400,200)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+  theme(text = element_text(size = 18))
+ggsave(paste(github_dir,'/figures/IC2_Full_Vascular.png',sep=''),dpi=300,width=180,height=120,units='mm')
+
+#####################################IC3#######################################
+master <- read.csv2(paste(github_dir,'Data/NASA_EMIT_Campaign_062024/Data_Sheets/Vascular/Datasheet_Vascular_IC3_06082024.csv',sep=''),sep=',',header=T)
+master$File<-str_pad(master$File, 5, pad = "0")
+dataset <- data.frame()
+
+for(i in 1:length(master$File)){
+  file <- list.files(paste(github_dir,'/Data/NASA_EMIT_Campaign_062024/IC3_veg',sep=''), full.names = T, pattern = paste(master$File[i],".asd",sep=''))
+  print(file)
+  md<-get_metadata(file)
+  temp_data<-get_spectra(file)
+  data<-as.numeric(temp_data)
+  wvl=as.integer(colnames(temp_data))
+  c1=data[which(wvl==1000)]-data[which(wvl==1001)]
+  c2=data[which(wvl==1800)]-data[which(wvl==1801)]
+  data_c=c(data[1:which(wvl==1000)],data[which(wvl==1001):which(wvl==1800)]+c1,data[which(wvl==1801):which(wvl==2500)]+c1+c2)
+  data_s=data_c/sqrt(sum(data_c^2))
+  type=rep(master$Plant[i],length(wvl))
+  
+  ds=cbind(wvl,data_c,type)
+  dataset <- rbind(dataset,ds)
+}
+
+#Name the columns
+colnames(dataset) <- c("wavelength","reflectance","type")
+dataset=transform(dataset,wavelength = as.numeric(wavelength))
+dataset=transform(dataset,reflectance = as.numeric(reflectance))
+
+#####ASD Reflectance Plots#################
+###Isolate plant signatures
+vascular<-subset(dataset, type %in% c('ARAR','CORA','EPTO','EPVI','JUOS','KRLA'))
+vascular_mean_std <- vascular %>%
+  group_by(wavelength,type) %>%
+  summarise_at(vars(reflectance), list(mean=mean, sd=sd)) %>%
+  as.data.frame()
+
+vascular_mean_std$type <- factor(vascular_mean_std$type,levels=c('KRLA','ARAR','CORA','EPTO','EPVI','JUOS')) #Set the order for manual fill
+ggplot(vascular_mean_std, aes(x=wavelength,y=mean,group=type,color=type)) +
+  geom_line(show.legend = T,linewidth=.5,linetype="solid") +
+  geom_ribbon(aes(y = mean, ymin = mean - sd, ymax = mean + sd, fill = type), alpha = .2) +
+  scale_y_continuous("Reflectance") +
+  scale_x_continuous("Wavelength (nm)",limits = c(400,2400), breaks = seq(400,2400,200)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+  theme(text = element_text(size = 18))
+ggsave(paste(github_dir,'/figures/IC3_Full_Vascular.png',sep=''),dpi=300,width=180,height=120,units='mm')
+
+#####################################SF#######################################
+master <- read.csv2(paste(github_dir,'Data/NASA_EMIT_Campaign_062024/Data_Sheets/Vascular/Datasheet_Vascular_SF_06102024.csv',sep=''),sep=',',header=T)
+master$File<-str_pad(master$File, 5, pad = "0")
+dataset <- data.frame()
+
+for(i in 1:length(master$File)){
+  file <- list.files(paste(github_dir,'/Data/NASA_EMIT_Campaign_062024/SF_veg',sep=''), full.names = T, pattern = paste(master$File[i],".asd",sep=''))
+  print(file)
+  md<-get_metadata(file)
+  temp_data<-get_spectra(file)
+  data<-as.numeric(temp_data)
+  wvl=as.integer(colnames(temp_data))
+  c1=data[which(wvl==1000)]-data[which(wvl==1001)]
+  c2=data[which(wvl==1800)]-data[which(wvl==1801)]
+  data_c=c(data[1:which(wvl==1000)],data[which(wvl==1001):which(wvl==1800)]+c1,data[which(wvl==1801):which(wvl==2500)]+c1+c2)
+  data_s=data_c/sqrt(sum(data_c^2))
+  type=rep(master$Plant[i],length(wvl))
+  
+  ds=cbind(wvl,data_c,type)
+  dataset <- rbind(dataset,ds)
+}
+
+#Name the columns
+colnames(dataset) <- c("wavelength","reflectance","type")
+dataset=transform(dataset,wavelength = as.numeric(wavelength))
+dataset=transform(dataset,reflectance = as.numeric(reflectance))
+
+#####ASD Reflectance Plots#################
+###Isolate plant signatures
+vascular<-subset(dataset, type %in% c('PIED'))
+vascular_mean_std <- vascular %>%
+  group_by(wavelength,type) %>%
+  summarise_at(vars(reflectance), list(mean=mean, sd=sd)) %>%
+  as.data.frame()
+
+ggplot(vascular_mean_std, aes(x=wavelength,y=mean,group=type,color=type)) +
+  geom_line(show.legend = T,linewidth=.5,linetype="solid") +
+  geom_ribbon(aes(y = mean, ymin = mean - sd, ymax = mean + sd, fill = type), alpha = .2) +
+  scale_y_continuous("Reflectance") +
+  scale_x_continuous("Wavelength (nm)",limits = c(400,2400), breaks = seq(400,2400,200)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+  theme(text = element_text(size = 18))
+ggsave(paste(github_dir,'/figures/SF_Full_Vascular.png',sep=''),dpi=300,width=180,height=120,units='mm')
